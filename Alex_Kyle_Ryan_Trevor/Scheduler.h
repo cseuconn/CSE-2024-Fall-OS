ec51391f4dc810cf575292b37169a9061d89a403
lily ✿ <patriotlandscapeipad@gmail.com>
2024-11-15 18:34:04 -0500

    Update Scheduler.h

--- Alex_Kyle_Ryan_Trevor/Scheduler.h
+++ Alex_Kyle_Ryan_Trevor/Scheduler.h
@@ -1,10 +1,11 @@
 #ifndef SCHEDULER_H  
 #define SCHEDULER_H
 
-
 void sort_by_arrival_time(struct process processes[], int n);
 void FIFO_Scheduling(struct process processes[], int n);
 void SJF_Scheduling(struct process processes[], int n);
 void RR_Scheduling(struct process processes[], int n, int time_quantum);
+void Priority_Scheduling(struct process processes[], int n);
+void MultilevelQueue_Scheduling(struct process processes[], int n);
 
 #endif
