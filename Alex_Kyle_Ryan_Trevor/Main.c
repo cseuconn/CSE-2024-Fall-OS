71b976d3972a781d106bc222321a3deb0d22b38d
lily ✿ <patriotlandscapeipad@gmail.com>
2024-11-15 18:32:58 -0500

    SJF
    Added SJF, fixed some spelling errors, added schedular_menu as separate function

--- Alex_Kyle_Ryan_Trevor/Main.c
+++ Alex_Kyle_Ryan_Trevor/Main.c
@@ -29,7 +29,7 @@ void sort_by_arrival_time(struct process processes[], int n){
     }
 }
 
-void FIFO_Scheduling(struct process processes[], int n){
+void FIFO_Scheduling (struct process processes[], int n){
     //Sort the processes by arrival time using insertion sort
     sort_by_arrival_time(processes, n);
 
@@ -56,7 +56,47 @@ void FIFO_Scheduling(struct process processes[], int n){
     }
 }
 
-void RR_Scheduling(struct process processes[], int n, int time_quantom){
+void SJF_Scheduling(struct process processes[], int n)
+{
+    sort_by_arrival_time(processes, n);
+    int current_time = 0;
+    
+    for (int i = 0; i < n; i++)
+    {
+        // Start at soonest possibility
+        if (current_time < processes[i].arrival_time)
+        {
+            printf("CPU idle from %d to %d\n", current_time, processes[i].arrival_time);
+            current_time = processes[i].arrival_time;
+        }
+        
+        // Find shortest process that has arrived and  not completed
+        int next = -1;
+        int j = 0;
+        while ((processes[j].arrival_time <= current_time) && (j < n))
+        {
+            if (!processes[j].is_completed)
+            {
+                if (next == -1) next = j;
+                else if (processes[j].burst_time < processes[next].burst_time) next = j;
+            }
+            
+            j++;
+        }
+        
+        //Calculate the current time after the next process completes
+        printf("Process %d starts at time %d\n", processes[j].pid, current_time);
+        current_time += processes[j].burst_time;
+        printf("Process %d ends at time %d\n", processes[j].pid, current_time);
+
+        //Calculate the process's completion, turnaround, and waiting time
+        processes[j].completion_time = current_time;
+        processes[j].turnaround_time = current_time - processes[j].arrival_time;
+        processes[j].waiting_time = processes[j].turnaround_time - processes[j].burst_time;
+    }
+}
+
+void RR_Scheduling(struct process processes[], int n, int time_quantum){
     //Sort the processes by arrival time using insertion sort
     sort_by_arrival_time(processes, n);
 
@@ -68,17 +108,17 @@ void RR_Scheduling(struct process processes[], int n, int time_quantom){
         //Start by assuming all processes are done
         done = 1;
 
-        //Iterate through processes to check next round
-        for(int i = 0; i < n; i++){
-            if(processes[i].remaining_time > 0){
+        //Itirate through processes to check next round
+        for (int i = 0; i < n; i++){
+            if (processes[i].remaining_time > 0){
                 //Not all processes are done
                 done = 0;
 
                 //Handle CPU idle time
-                if(processes[i].arrival_time > current_time){
+                if (processes[i].arrival_time > current_time){
                     //Find the earliest arrival time of remaining process
                     int earliest_arrival = processes[i].arrival_time;
-                    for(int j = 0; j < n; j++){
+                    for (int j = 0; j < n; j++){
                         if (processes[j].remaining_time > 0 && processes[j].arrival_time < earliest_arrival) {
                             earliest_arrival = processes[j].arrival_time;
                         }
@@ -88,17 +128,17 @@ void RR_Scheduling(struct process processes[], int n, int time_quantom){
                     current_time = earliest_arrival;
                 }
 
-                //Exacute process for the time quantom or remaing time
-                if(processes[i].arrival_time <= current_time){
-                    int time_to_exacute = processes[i].remaining_time < time_quantom? processes[i].remaining_time:time_quantom;
+                //Execute process for the time quantum or remaining time
+                if (processes[i].arrival_time <= current_time){
+                    int time_to_exacute = processes[i].remaining_time < time_quantum? processes[i].remaining_time:time_quantum;
                     //Print process exacuting and current time 
                     printf("Process %d executes for %d units from time %d\n", processes[i].pid, time_to_exacute, current_time);
 
-                    //Update remainging and current time
+                    //Update remaining and current time
                     current_time += time_to_exacute;
                     processes[i].remaining_time -= time_to_exacute;
                     
-                    if(processes[i].remaining_time == 0){
+                    if (processes[i].remaining_time == 0){
                         processes[i].completion_time = current_time;
                         printf("Process %d completes at time %d\n", processes[i].pid, current_time);
                     }
@@ -112,6 +152,48 @@ void RR_Scheduling(struct process processes[], int n, int time_quantom){
     for(int i = 0; i < n; i++){
         processes[i].turnaround_time = processes[i].completion_time - processes[i].arrival_time;
         processes[i].waiting_time = processes[i].turnaround_time - processes[i].burst_time;
+    }
+}
+
+void Schedular_Menu(void)
+{
+    printf("Choose a schedular from the following list:\n");
+    printf("1. First In First Out (FIFO)\n");
+    printf("2. Shortest Job First (SJF)\n");
+    printf("3. Round Robin (RR)\n");
+    printf("4. Priority Scheduling\n");
+    printf("5. Multilevel Queue Scheduling\n");
+    int schedular_choice;
+    scanf("%d", &schedular_choice);
+
+    switch (schedular_choice)
+    {
+        case 1:
+            printf("\nFirst In First Out (FIFO) Schedular\n");
+            FIFO_Scheduling(proc, n);
+            break;
+        case 2:
+            printf("\nShortest Job First (SJF) Schedular\n");
+            SJF_Scheduling(proc, n);
+            break;
+        case 3:
+            printf("\nRound Robin (RR) Schedular\n");
+            printf("Enter the time quantum: ");
+            int quantum;
+            scanf("%d", &quantum);
+            RR_Scheduling(proc, n, quantum);
+            break;
+        case 4:
+            printf("\nPriority Scheduling Schedular\n");
+            Priority_Scheduling(proc, n);
+            break;
+        case 5:
+            printf("\nMultilevel Queue Scheduling Schedular\n");
+            MultilevelQueue_Scheduling(proc, n);
+            break;
+        default:
+            printf("Invalid choice\n");
+            break;
     }
 }
 
@@ -146,87 +228,11 @@ int main(int argc, char *argv[]) {
         printf("\n");
 
         for (int i = 0; i < num_schedulers; i++){
-            printf("Choose the schedular from the following list:\n");
-            printf("1. First Come First Serve (FCFS)\n");
-            printf("2. Shortest Job First (SJF)\n");
-            printf("3. Round Robin (RR)\n");
-            printf("4. Priority Scheduling\n");
-            printf("5. Multilevel Queue Scheduling\n");
-            int schedular_choice;
-            scanf("%d", &schedular_choice);
-
-            void (*scheduler_functions[]) (struct process*, int) = {FCFS, SJF, RR, Priority, MultilevelQueue};
-
-            switch (schedular_choice) {
-                case 1:
-                    printf("\nFirst Come First Serve (FCFS) Schedular\n");
-                    FCFS(proc, n);
-                    break;
-                case 2:
-                    printf("\nShortest Job First (SJF) Schedular\n");
-                    SJF(proc, n);
-                    break;
-                case 3:
-                    printf("\nRound Robin (RR) Schedular\n");
-                    printf("Enter the time quantum: ");
-                    int quantum;
-                    scanf("%d", &quantum);
-                    RR(proc, n, quantum);
-                    break;
-
-                case 4:
-                    printf("\nPriority Scheduling Schedular\n");
-                    Priority(proc, n);
-                    break;
-                case 5:
-                    printf("\nMultilevel Queue Scheduling Schedular\n");
-                    MultilevelQueue(proc, n);
-                    break;
-                default:
-                    printf("Invalid choice\n");
-                    break;
-            }
+            Schedular_Menu();
         }
     } 
     else {
-        printf("Choose a schedular from the following list:\n");
-        printf("1. First Come First Serve (FCFS)\n");
-        printf("2. Shortest Job First (SJF)\n");
-        printf("3. Round Robin (RR)\n");
-        printf("4. Priority Scheduling\n");
-        printf("5. Multilevel Queue Scheduling\n");
-        int schedular_choice;
-        scanf("%d", &schedular_choice);
-
-        switch (schedular_choice) {
-            case 1:
-                printf("\nFirst Come First Serve (FCFS) Schedular\n");
-                FCFS(proc, n);
-                break;
-            case 2:
-                printf("\nShortest Job First (SJF) Schedular\n");
-                SJF(proc, n);
-                break;
-            case 3:
-                printf("\nRound Robin (RR) Schedular\n");
-                printf("Enter the time quantum: ");
-                int quantum;
-                scanf("%d", &quantum);
-                RR(proc, n, quantum);
-                break;
-                
-            case 4:
-                printf("\nPriority Scheduling Schedular\n");
-                Priority(proc, n);
-                break;
-            case 5:
-                printf("\nMultilevel Queue Scheduling Schedular\n");
-                MultilevelQueue(proc, n);
-                break;
-            default:
-                printf("Invalid choice\n");
-                break;
-        }
+        Schedular_Menu();
     }
 
     return 0;
