da86608ff1a0422e697e9b6aa7a06412d5aabc13
Cecil <cecil.ho@uconn.edu>
2024-11-15 13:58:47 -0500

    added prints after running fifo

--- Mia_Brian_Liya_Cecil/main.c
+++ Mia_Brian_Liya_Cecil/main.c
@@ -7,5 +7,17 @@
 
 int main(int argc, char *argv[])
 {
-    job_info job_list[5];
+    struct thread_info job_list[5];
+    int job_expected_times[5][2];
+    int scheduler;
+
+    printf('Running FIFO tests\n');
+    fifo_in_place(&job_list);
+    for (int i = 0; i < 5; i++)
+    {
+        printf('Job %d expected turnaround time: %d, actual turnaround time: %d',
+                i, job_list[i].turnaround_time, job_expected_times[i][0]);
+        printf('Job %d expected response time: %d, actual response time: %d',
+                i, job_list[i].response_time, job_expected_times[i][1]);
+    }
 }
