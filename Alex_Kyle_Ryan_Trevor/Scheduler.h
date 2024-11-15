bf422d9914edbe248059ae485119481edb82d48d
lily ✿ <patriotlandscapeipad@gmail.com>
2024-11-15 18:13:20 -0500

    Add SJF

--- Alex_Kyle_Ryan_Trevor/Scheduler.h
+++ Alex_Kyle_Ryan_Trevor/Scheduler.h
@@ -4,6 +4,7 @@
 
 void sort_by_arrival_time(struct process processes[], int n);
 void FIFO_Scheduling(struct process processes[], int n);
+void SJF_Scheduling(struct process processes[], int n);
 void RR_Scheduling(struct process processes[], int n, int time_quantum);
 
 #endif
