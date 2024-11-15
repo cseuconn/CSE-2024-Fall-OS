ad51dec830fa9544f6f6f08d7da2104ccd065e0a
KHM20002 <kyle.madsen@uconn.edu>
2024-11-15 16:55:11 -0500

    Added FIFO, RR, and arrival time sort to  Scheduler.h

--- Alex_Kyle_Ryan_Trevor/Scheduler.h
+++ Alex_Kyle_Ryan_Trevor/Scheduler.h
@@ -1,1 +1,9 @@
+#ifndef SCHEDULER_H  
+#define SCHEDULER_H
 
+
+void sort_by_arrival_time(struct process processes[], int n);
+void FIFO_Scheduling(struct process processes[], int n);
+void RR_Scheduling(struct process processes[], int n, int time_quantum);
+
+#endif
