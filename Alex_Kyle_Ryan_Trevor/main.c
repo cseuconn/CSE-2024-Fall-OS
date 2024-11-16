65fe90357761f89713b0efec69ac2194b34f98eb
lily ✿ <patriotlandscapeipad@gmail.com>
2024-11-15 19:52:12 -0500

    Update main.c

--- Alex_Kyle_Ryan_Trevor/main.c
+++ Alex_Kyle_Ryan_Trevor/main.c
@@ -77,6 +77,7 @@ int main(int argc, char *argv[]) {
         for (int i = 0; i < num_schedulers; i++) {
             Scheduler_Menu(proc, n);
         } 
+    }
     else {
         Scheduler_Menu(proc, n);
     }
