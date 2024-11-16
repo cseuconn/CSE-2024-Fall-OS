644f226f47ab75cb3d883dcdc62136586a07f80f
Cecil <cecil.ho@uconn.edu>
2024-11-16 03:31:20 -0500

    added fifo scheduler

--- Mia_Brian_Liya_Cecil/main.c
+++ Mia_Brian_Liya_Cecil/main.c
@@ -1,9 +1,57 @@
 #include "thread.h"
-#include "FIFO.c"
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <assert.h>
+
+// a function that sorts the jobs by arrival time using bubble sort
+void sort_arrival_time(struct thread_info* jobs_array, int numJobs)
+{
+    for ( int i = 0; i < numJobs - 1; i++)
+    {
+        for ( int j = 0; j < numJobs - i - 1; j++ )
+        {
+            if (jobs_array[j].arrival_time > jobs_array[j+1].arrival_time)
+            {
+                // swap the jobs
+                struct thread_info temp = jobs_array[j];
+                jobs_array[j] = jobs_array[j+1];
+                jobs_array[j+1] = temp;
+            }
+        }
+    }
+}
+
+
+void fifo_in_place(struct thread_info* jobs_array)
+{
+    // get the number of jobs in the array
+    int numJobs = sizeof(jobs_array) / sizeof(jobs_array[0]);
+    
+    // sort the jobs based on arrival time using a bubble sort
+    sort_arrival_time(jobs_array, numJobs);
+
+    int curr_time = 0;
+
+    // process the jobs through FIFO
+    for ( int i = 0; i < numJobs; i++)
+    {
+        // get the job
+        struct thread_info* job = &jobs_array[i];
+
+        // wait for the job to arrive and go to that specific time
+        if ( curr_time < job->arrival_time )
+        {
+            curr_time = job->arrival_time;
+        }
+
+        // set the completion time
+        job->completion_time = curr_time + job->time_remaining;   
+
+        // update the current time
+        curr_time = job->completion_time;     
+    }
+}
 
 void setup(struct thread_info *jobs, int * job_info)
 {
@@ -37,7 +85,7 @@ int main(int argc, char *argv[])
     {
         printf("Job %d expected turnaround time: %d, actual turnaround time: %d\n",
                 i, job_expected_times_fifo[i][0], job_list[i].turnaround_time);
-        printf("Job %d expected response time: %d, actual response time: %d\n",
+        printf("Job %d expected response time: %d, actual response time: %d\n\n",
                 i, job_expected_times_fifo[i][1], job_list[i].response_time);
     }
 
@@ -48,7 +96,7 @@ int main(int argc, char *argv[])
     {
         printf("Job %d expected turnaround time: %d, actual turnaround time: %d\n",
                 i, job_expected_times_sfj[i][0], job_list[i].turnaround_time);
-        printf("Job %d expected response time: %d, actual response time: %d\n",
+        printf("Job %d expected response time: %d, actual response time: %d\n\n",
                 i, job_expected_times_sfj[i][1], job_list[i].response_time);
     }
 
