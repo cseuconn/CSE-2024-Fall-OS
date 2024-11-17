f4edf0c72f1b033f9b5375b9cafa1b4f186b1a32
Mia Larkin <122827012+mialarkin14@users.noreply.github.com>
2024-11-16 19:21:45 -0500

    Update FIFO.c

--- Mia_Brian_Liya_Cecil/FIFO.c
+++ Mia_Brian_Liya_Cecil/FIFO.c
@@ -45,9 +45,10 @@ void fifo_in_place(struct thread_info* jobs_array)
             curr_time = job->arrival_time;
         }
 
-        // set the completion time
+         // set the completion time and first run time
+        job->first_run = curr_time;
         job->completion_time = curr_time + job->time_remaining;   
-
+        
         // update the current time
         curr_time = job->completion_time;     
     }
